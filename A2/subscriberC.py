03562e9100087bec0c1b14e22882c6f2c1795241
jlmizug <jlmizug@hotmail.com>
2023-09-06 16:21:51 -0300

    refactor: change comments

--- A2/subscriberC.py
+++ A2/subscriberC.py
@@ -1,25 +1,25 @@
 import paho.mqtt.client as mqtt
 import json
 
-# Callback running on connection
+# Método é chamado quando a conexão é realizada
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    # After connection we subscribe to the "$SYS/#" topics (internal topics that will produce lot of data, perfect for our example)
+    # Se inscreve no tópico especifico
     print("Inscrito no topico ELECTRONICS")
     client.subscribe("ELECTRONICS")
     print("Inscrito no topico HOUSEHOLD")
     client.subscribe("HOUSEHOLD")
 
 
-# Callback running on new message
+# Método é chamado quando uma mensagem é recebida
 def on_message(client, userdata, msg):
-    # We print each message received
+    # Tratamos o pacote recebido
     mensagem = json.loads(msg.payload.decode("utf-8","ignore"))
-    
+    # Escrevemos no terminal a mensagem recebida
     print("Teste, temos uma nova promocao de " + mensagem.get("category") + " nas lojas " + mensagem.get("store") + " por apenas " 
     + str(mensagem.get("price")) + " reais.")
 
-# Initiate the MQTT client
+# Inicia o MQTT Client
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
